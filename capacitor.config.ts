a6497e6be20814b64d3a2aade0bc86ae7f003457
German <gerasman827@gmail.com>
2026-09-11 20:37:22 -0500

    config. capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.edu.autonoma.electiva202603',
+  appName: 'ElevativaI',
+  webDir: 'www'
+};
+
+export default config;
